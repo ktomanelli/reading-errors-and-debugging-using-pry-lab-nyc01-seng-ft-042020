7afada6f31f8a150909d62fda3c0f2d5e815a115
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-04-08 18:06:56 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,7 +2,9 @@
 
 def snake_it_up(string)
   if string[0] == "s"
-     string = 10.Times("s") + string
+    newString=''
+     10.Times(newString+="s") 
+     newString+=string
   else
     string
   end
